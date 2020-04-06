03dadd4bc1b638191f3821cea5390e8fb8afb72d
loyacks87 <cb.loyacks87@gmail.com>
2020-04-06 14:45:59 +0800

    edit js

--- cyril.js
+++ cyril.js
@@ -10,7 +10,14 @@ const talent = [
     "at sex"
 ];
 
+const talent1 = [
+    "manyakis",
+    "uwagan",
+    "kiatan",
+];
+
 const randomtalent = (talent) => talent[Math.floor(Math.random()*talent.length)];
+const randomtalent1 = (talent1) => talent1[Math.floor(Math.random()*talent1.length)];
 
 const button = document.getElementById("enter");
 const input = document.getElementById("insertname");
@@ -25,7 +32,16 @@ button.addEventListener('click', function() {
     const div1 = document.createElement("div");
     div1.className = "card card-body";
     const par = document.createElement("p");
-    const node = document.createTextNode(`${input.value}, you are a good ${randomtalent(talent)}!`);        
+    const node = document.createTextNode(`${input.value}, you are a good ${randomtalent(talent)}!`);
+    const node1 = document.createTextNode(`${input.value}, ${randomtalent1(talent1)}!`);    
+    
+    if(input.value === 'ryan', 'Ryan', 'RYAN', 'maq', 'mak', 'Maq', 'Mak', 'MAQ', 'MAK') {
+        par.appendChild(node1);
+        div1.appendChild(par);
+        rem = div.appendChild(div1);
+        input.value = "";
+    }
+    
     if(input.value.length >0) {
         par.appendChild(node);
         div1.appendChild(par);
